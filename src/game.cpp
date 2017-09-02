de95c2b4bad35092b55688c4c9f392214983259f
Alaroff <alar0330@yahoo.com>
2017-09-02 16:50:50 +0200

    Deleted curses libraries, added .zip

--- src/game.cpp
+++ src/game.cpp
@@ -1,9 +1,22 @@
-#ifdef _WIN32
-  #include "../inc/ncursesw/ncurses.h"
-#elif __linux__
-  #include "../inc/ncurses/curses.h"
+// if compile with destributed copy of ncurses library
+#ifdef GIVEMEYOURCURSES
+  #ifdef _WIN32
+    #include "../inc/ncursesw/ncurses.h"
+  #elif __linux__
+    #include "../inc/ncurses/curses.h"
+  #else
+    // other systems
+  #endif
+
+// if compile with already pre-installed libraries of ncurses
 #else
-  // other systems
+  #ifdef _WIN32
+    #include <ncursesw/ncurses.h>
+  #elif __linux__
+    #include <ncurses/ncurses.h>
+  #else
+    // other systems
+  #endif
 #endif
 
 #include "../inc/nsnake.hpp"
